7b4b2728bef5c329981016700ba67e7622cba6cc
Pongpat Weesommai <empongpat@gmail.com>
2016-12-06 18:04:57 +0800

    Add datepicker interval structure

---
+++ lib/src/components/interval/datepicker_interval_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular2/core.dart';
+
+@Component(
+  selector: 'datepicker-interval',
+  templateUrl: 'datepicker_interval_component.html',
+  styleUrls: const['datepicker_interval_component.css']
+)
+
+class DatepickerIntervalComponent {
+
+}
